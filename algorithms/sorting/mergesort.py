c19ad541ebe8771ee4728224c42489573b106be2
Kislov Dmitry <kislov@easydan.com>
2018-10-16 09:56:35 +1000

    Merge sort: done

--- algorithms/sorting/mergesort.py
+++ algorithms/sorting/mergesort.py
@@ -3,6 +3,26 @@ __created__ = "15.10.2018"
 __email__ = "kislov@easydan.com"
 
 
+def test_combine():
+    ''' This function isn't a part of the algorithm.
+        It is just for testing purposes.
+    '''
+
+    print('Testing combine function ... ')
+    x = [1, 5, 12, 90]
+    y = [-2, -1, 0,  20, 21, 22, 110, 160, 500]
+    print("Combining x, y...", x, y)
+    result1 = combine(x, y)
+    print("Combining y, x...", y, x)
+    result2 = combine(y, x)
+    if result1 == result2:
+        print("combine(x, y) == combine(y, x)!")
+        print("The test for the combine function is passed!")
+    else:
+        print("combine(x, y) != combine(y, x)!")
+        print("Something went wrong when executing the combine function!")
+
+
 def combine(left, right):
     combined = []
     left = left[::-1]
@@ -22,23 +42,18 @@ def combine(left, right):
         combined.extend(right[::-1])
     return combined
 
-def test_combine():
-    print('Testing combine function ... ')
-    x = [1, 5, 12, 90]
-    y = [-2, -1, 0,  20, 21, 22, 110, 160, 500]
-    print("Combining x, y", x, y)
-    result1 = combine(x, y)
-    print("Combining y, x", y, x)
-    result2 = combine(y, x)
-    if result1 == result2:
-        print("Test for the combine function is passed!")
-    else:
-        print("Something is wrong with the combine function!")
 
-def merge_sort(thelist, result=[]):
+def merge_sort(thelist, result=[], order='asc'):
     '''
-    Sorting by array merging.
-    NOT COMPLETED YET!!!!
+    Sorting arrays by merging.
+
+    **Parameters**
+
+        :param thelist: an iterable to be sorted (tested with lists)
+        :param result: an array for result storing
+        :param order: a string, ordering option, default is 'asc'
+        :returns: sorted list of input values
+        :rtype: list
 
     '''
     n = len(thelist)
@@ -49,18 +64,24 @@ def merge_sort(thelist, result=[]):
     else:
         left = thelist[:n//2]
         right = thelist[n//2:]
-    l, r = merge_sort(left, result=[])
+    l, r = merge_sort(left, result=[], order=order)
     combined_l = combine(l, r)
-    l, r = merge_sort(right, result=[])
+    l, r = merge_sort(right, result=[], order=order)
     combined_r = combine(l, r)
-    print("Combining... ", combined_l, combined_r)
     result.extend(combine(combined_l, combined_r))
+    if order != 'asc':
+        result = result[::-1]
     return combined_l, combined_r
 
 
 if __name__ == '__main__':
     test_combine()
     array_reversed = list(range(10)[::-1])
+    print("Array to be sorted: ", array_reversed)
     result = []
-    l1, l2 = merge_sort(array_reversed, result=result)
+    merge_sort(array_reversed, result=result)
+    print('Result is ', result)
+    simple_array = list(range(10))
+    result = []
+    merge_sort(simple_array, result=result)
     print('Result is ', result)
