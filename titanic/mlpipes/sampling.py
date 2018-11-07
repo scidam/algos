dda82dadc449da6314484544fad4c821484140f5
Kislov Dmitry <kislov@easydan.com>
2018-11-07 14:57:28 +1000

    sample_like_target: tested on numerical data

--- titanic/mlpipes/sampling.py
+++ titanic/mlpipes/sampling.py
@@ -3,6 +3,7 @@ from .pfunc import check_type
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from scipy.spatial.distance import pdist, squareform
 
 try:
     import imblearn
@@ -13,7 +14,9 @@ except ImportError:
 
 @check_type()
 def sample_like_target(target, source, using=None, autoencode=False,
-                       scale=True, random_state=None, fillna=True):
+                       scale=True, random_state=None, fillna=False,
+                       distance=lambda x: pdist(x, 'euclidean'),
+                       quantile_use=0.1):
 
     if isinstance(using, Iterable):
         using = list(set(using))
@@ -28,10 +31,12 @@ def sample_like_target(target, source, using=None, autoencode=False,
         S.fillna(S.median(), inplace=True)
     else:
         dropped_mask = pd.isnull(S).sum(axis=1).astype(np.bool)
-        S = S.iloc[~dropped_mask, :]
+        leaved_indices = [j for j in range(S.shape[0]) if not dropped_mask[j]]
+        S = S.loc[~dropped_mask, :]
         T = T.dropna()
 
     if autoencode:
+        print(type(T), type(S))
         C = pd.concat([T, S]).reset_index(drop=True)
         R = pd.get_dummies(C, dummy_na=True)
         T = R.iloc[:T.shape[0], :]
@@ -40,17 +45,26 @@ def sample_like_target(target, source, using=None, autoencode=False,
     if scale:
         scaler = StandardScaler()
         scaler.fit(T)
-        T = scaler.transform(T)
-        S = scaler.transform(S)
-
-
-
-
+        tc = T.columns
+        sc = S.columns
+        T = pd.DataFrame(scaler.transform(T.values), columns=tc)
+        S = pd.DataFrame(scaler.transform(S.values), columns=sc)
     combined = pd.concat([T, S]).reset_index(drop=True)
-    y = [0] * T.shape[0] + [1] * S.shape[0]
+    distances = squareform(distance(combined))[:T.shape[0],:]
+    quantiles =  np.percentile(distances, quantile_use * 100, axis=1)
+    repeated = np.repeat(quantiles[:,np.newaxis], S.shape[0], axis=1)
+    filter_mask = distances[:, T.shape[0]:] <= repeated
+    to_leave = np.unique(np.repeat([leaved_indices], T.shape[0], axis=0)[filter_mask])
+    return source.iloc[to_leave.tolist(), :]
 
-    tmklnk = imblearn.under_sampling.TomekLinks([1], random_state=random_state)
-    tmklnk.fit_resample(combined, y)
-    return S.iloc[tmklnk.sample_indices_, :]
+
+if __name__ == '__main__':
+    target = pd.DataFrame(np.random.rand(100, 2)+0.8)
+    source = pd.DataFrame(np.random.rand(400, 2))
+    result = sample_like_target(target, source)
+    import matplotlib.pyplot as plt
+    plt.plot(target.values[:,0], target.values[:,1],'ro', source.values[:,0], source.values[:,1], 'b.',
+         result.values[:,0], result.values[:,1], 'gx')
+    plt.show()
 
 
