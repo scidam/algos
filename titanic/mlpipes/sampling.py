55a69c7cf098724f4058b67b3aa59473ca9e7c4f
Kislov Dmitry <kislov@easydan.com>
2018-11-07 10:39:54 +1000

    Sampling isn't completed yet

--- titanic/mlpipes/sampling.py
+++ titanic/mlpipes/sampling.py
@@ -1,10 +1,7 @@
-
 from collections.abc import Iterable
 from .pfunc import check_type
 import numpy as np
 import pandas as pd
-
-from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
 
 try:
@@ -13,9 +10,9 @@ except ImportError:
     raise Exception("Sampling package depends on imbalanced-learning package."
                     "Install imbalanced-learning package first.")
 
+
 @check_type()
-def sample_like_target(target, source, using=None, target_outliers_ratio=0.05,
-                       target_max_samples=100, autoencode=False,
+def sample_like_target(target, source, using=None, autoencode=False,
                        scale=True, random_state=None, fillna=True):
 
     if isinstance(using, Iterable):
@@ -26,11 +23,9 @@ def sample_like_target(target, source, using=None, target_outliers_ratio=0.05,
     T = target.copy().loc[:, using]
     S = source.copy().loc[:, using]
 
-
     if fillna:
         T.fillna(T.median(), inplace=True)
         S.fillna(S.median(), inplace=True)
-
     else:
         dropped_mask = pd.isnull(S).sum(axis=1).astype(np.bool)
         S = S.iloc[~dropped_mask, :]
@@ -47,15 +42,6 @@ def sample_like_target(target, source, using=None, target_outliers_ratio=0.05,
         scaler.fit(T)
         T = scaler.transform(T)
         S = scaler.transform(S)
-
-    if target_outliers_ratio > 0.0:
-        target_dropper = IsolationForest(max_samples=target_max_samples,
-                                         contamination=target_outliers_ratio,
-                                         random_state=random_state,
-                                         behaviour='new')
-        target_dropper.fit(T)
-        outlier_mask = target_dropper.predict(T) == -1
-        T = T.iloc[outlier_mask, :]
 
 
 
