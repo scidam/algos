b714c08fb2c7eefacab3c14a30e01ce2b5fea071
Dmitry E. Kislov <kislov@easydan.com>
2019-01-03 12:15:02 +1000

    Non-recursive binary search implemented

--- algorithms/search/binary.py
+++ algorithms/search/binary.py
@@ -4,7 +4,7 @@ __email__ = "kislov@easydan.com"
 
 
 def binary_search(x, array):
-    '''Binary search algorithm. Recursive implementation.
+    """Binary search algorithm. Recursive implementation.
 
     **Parameters**
 
@@ -13,11 +13,17 @@ def binary_search(x, array):
        :returns: the found object; if the object doesn't belong to the array
                  returns None
        :rtype: the same type as x or None
-    
+
     **Complexity**
+
         :math:`O(log(n))`
 
-    '''
+    **Note**
+
+        It is assumed that 0-indexing system is used
+
+    """
+
     if len(array) == 0:
         return None
     if len(array) == 1:
@@ -43,6 +49,50 @@ def binary_search(x, array):
     return res
 
 
+def binary_search_nonrecursive(x, array):
+    """Non-recursive version of the binary search algorithm
+
+    **Parameters**
+
+        :param x: an object to be found
+        :param array: an array like object where `x` object will be looked for
+        :returns: an index of the object (if the elemnt was found) or -1
+        :rtype: int
+
+    """
+
+    done = False
+    _array = array[:]
+    index = 0
+    while not done:
+        if len(_array) > 2:
+            n = len(_array) // 2
+            if x > _array[:n][-1]:
+                index += n
+                _array = _array[n:]
+            else:
+                _array = _array[:n]
+        elif len(_array) == 2:
+            done = True
+            if x != _array[1] and x != _array[0]:
+                index = -1
+            elif x == _array[1]:
+                index += 1
+            elif x == _array[0]:
+                index += 0
+        elif len(_array) == 1:
+            if x != _array[0]:
+                index = -1
+            done = True
+        else:
+            index = -1
+            done = True
+
+    return index
+
+
 if __name__ == "__main__":
     data = sorted([1, 3, 5, 3, 2, 1, 5, 6, 7, 8, 4, 3, 2, 1, 5])
+    print("Source dataset is ", data)
     print("Result is ", binary_search(30, data))
+    print("Result is ", binary_search_nonrecursive(8, data))
