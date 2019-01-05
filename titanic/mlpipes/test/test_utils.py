682ed2ea044ac6b4097516399c2ddf12adc7938e
Dmitry E. Kislov <kislov@easydan.com>
2019-01-05 10:58:09 +1000

    Extra test added

--- titanic/mlpipes/test/test_utils.py
+++ titanic/mlpipes/test/test_utils.py
@@ -6,3 +6,10 @@ def test_iterate_over_pars():
                   'two': {'z': [3, 4]}}
     # dirty test
     assert len(list(iterate_over_pars(parameters))) == 6
+
+
+def test_iterate_over_pars_restricted():
+    parameters = {'one': {'x': [1, 2, 3], 'y': [8]},
+                  'two': {'z': [3, 4]}}
+    # dirty test
+    assert len(list(iterate_over_pars(parameters, max_iterations=3))) == 4
