a1b3270d0b4bfd21a8120275c703d817b7e1e68e
Dmitry Kislov <kislov@easydan.com>
2019-11-28 14:57:47 +1000

    MaxProd (positive numbers only)

---
+++ algorithms/intervieweing.io/maxprodsubset.py
@@ -0,0 +1,15 @@
+from math import log, exp
+
+# Max product of positive numbers (using log and exponentiation)
+# lets reduce the problem to finding max-sum
+
+arr = [3, 4, 1, 5, 0]
+
+def maxprod_subset(arr):
+    _arr = list(filter(lambda x: x != 0 and x != 1, arr))
+    print(_arr)
+    _arr = list(map(log, _arr))
+    _arr = list(filter(lambda x: x > 0, _arr))
+    return exp(sum(_arr))
+
+print(maxprod_subset(arr))  # That almost exact answer.
