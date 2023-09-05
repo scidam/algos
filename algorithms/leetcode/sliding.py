eb50bbdc716619719c7cdad6aa537998045c6262
Dmitry E. Kislov <kislov@easydan.com>
2023-09-05 21:43:51 +0300

    not solved problem

--- algorithms/leetcode/sliding.py
+++ algorithms/leetcode/sliding.py
@@ -1,15 +1,20 @@
-
 class Solution:
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        if k == 1:
+            return nums
         window = nums[:k]
         within_max = max(window)
         within_max_1 = max(window[1:])
         result = [within_max]
-        for i in range(1, len(nums)-k+1):
-            within_max = max(nums[i-1], within_max_1, nums[i+k-1])
-            within_max_1 = max(within_max_1, nums[i+k-1])
+        for i in range(1, len(nums) - k + 1):
+            within_max = max(nums[i - 1], within_max_1, nums[i + k - 1])
+            within_max_1 = max(within_max_1, nums[i + k - 1])
             result.append(within_max)
         return result
 
+
+# NOTE SOLVED!
+
+
 s = Solution()
-print(s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
+print(s.maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
