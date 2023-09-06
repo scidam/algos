0f93085dff28f21319d07e8d77634b37b093981d
Dmitry E. Kislov <kislov@easydan.com>
2023-09-06 18:49:51 +0300

    add some solutions

--- algorithms/leetcode/sliding.py
+++ algorithms/leetcode/sliding.py
@@ -5,15 +5,30 @@ class Solution:
         window = nums[:k]
         within_max = max(window)
         within_max_1 = max(window[1:])
+        within_max_both = max(window[1:-1]) if window[1:-1] else min(nums)
         result = [within_max]
         for i in range(1, len(nums) - k + 1):
-            within_max = max(nums[i - 1], within_max_1, nums[i + k - 1])
+            window = nums[i:i+k]
+            if window[-1] > within_max_1:
+                within_max_1 = nums[-1]
+                within_max = within_max_1
+                
+            elif within_max_1
+            
             within_max_1 = max(within_max_1, nums[i + k - 1])
             result.append(within_max)
         return result
 
 
 # NOTE SOLVED!
+
+
+[1, 3, -1, -3, 5, 3, 6, 7]
+
+
+ [1, 3, 5, -1]  => (window[0], max(widnow[1:]), max(window[1:-1]))
+     [3 5  -1 -3]  (window[0]=nums[i], if nums[i+k]>mw1: mw1=nums[i+k]; if nums[i+k]<mw1: ,)
+
 
 
 s = Solution()
