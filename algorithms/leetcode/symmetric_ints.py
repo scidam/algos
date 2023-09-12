2ef566496a5994cc3d31a41dbf9c1b7e558ac573
Dmitry E. Kislov <kislov@easydan.com>
2023-09-12 07:16:59 +0300

    add algo

--- algorithms/leetcode/symmetric_ints.py
+++ algorithms/leetcode/symmetric_ints.py
@@ -1,9 +1,12 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
-        s = map(str, range(low, high + 1))
-        fs = filter(
-            lambda x: len(x) % 2 == 0
-            and sum(map(int, x[: len(x) // 2])) == sum(map(int, x[len(x) // 2 :])),
-            s,
+        return len(
+            list(
+                filter(
+                    lambda x: len(x) % 2 == 0
+                    and sum(map(int, x[: len(x) // 2]))
+                    == sum(map(int, x[len(x) // 2 :])),
+                    map(str, range(low, high + 1)),
+                )
+            )
         )
-        return len(list(fs))
