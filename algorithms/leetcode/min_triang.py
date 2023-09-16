8942a2d27dcb0506e6d1e8aa7dd7f9f9aa6c8027
Dmitry E. Kislov <kislov@easydan.com>
2023-09-16 10:46:09 +0300

    algo done

---
+++ algorithms/leetcode/min_triang.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimumTotal(self, triangle: list[List[int]]) -> int:
+        from copy import deepcopy
+        k, i = 0 , len(triangle) - 1 
+        results = []
+        for k in range(len(triangle[-1])):
+            sum = triangle[-1][k]
+            _k = k 
+            for i in range(len(triangle), -1, -1):
+                if triangle[i][_k-1] < 
+                sum += min(triangle[i][_k-1], triangle[i][_k])
+        
+            
+        
+
+# i, k<i
+
+# i-1:   m(k) = min(m(k-1), m(k))
+# i: m(k) -- minimal position, 
